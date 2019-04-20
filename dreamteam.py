6bf13623107a3fdc4a3b0f7755e6a29ebe8a47f1
Manish Chalana <ManishChalana@Manishs-MacBook-Air.local>
2019-04-20 11:25:30 +0530

    Cleaned up dreamteam.py file; no code change

--- dreamteam.py
+++ dreamteam.py
@@ -30,22 +30,17 @@ class Dream11(Team):
         self.playerTeamMap = {p['name']:p['team'] for p in self.players}
         self.playerTypeMap = {p['name']:p['type'] for p in self.players}
         self.playerTypeCount = collections.Counter(self.playerTypeMap.values())
-        
-        
+                
     @staticmethod
-    def sample(players, req):
-        
+    def sample(players, req):        
         numStars = np.sum([p['star'] for p in players])
-
         availableStars = [p['name'] for p in players if p['star']==1]
-
         availableNonStars = [p['name'] for p in players if not(p['star']==1)]
     
         if numStars >= req:
             return ([] if len(availableStars)==0 else rnd.sample(availableStars, req))
         else:
-            return availableStars + rnd.sample(availableNonStars, req - numStars)
-        
+            return availableStars + rnd.sample(availableNonStars, req - numStars)        
     
     def generateTeams(self, numTeams, captain=False):
         selectedTeams = []
@@ -56,44 +51,26 @@ class Dream11(Team):
                 [numWks, numBats, numArs, numBowls] = rnd.sample(self.combinations, 1)[0]
                 if (numWks <= self.playerTypeCount['wk']) and (numBats <=self.playerTypeCount['bat']) and (numArs <=self.playerTypeCount['ar']) and (numBowls<=self.playerTypeCount['bowl']):
                     break
-                
-                
+                    
             wks = Dream11.sample([p for p in self.players if p['type']=='wk'], numWks)
-            
-            
             bats = Dream11.sample([p for p in self.players if p['type']=='bat'], numBats)
-            
-            
             ars = Dream11.sample([p for p in self.players if p['type']=='ar'], numArs)
-            
-            
             bowls = Dream11.sample([p for p in self.players if p['type']=='bowl'], numBowls)
-            
-            
             selectedTeam = wks + bats + ars + bowls
- 
             
             teamDemography = collections.Counter([self.playerTeamMap[player] for player in selectedTeam])
-            
             totalPoints = np.sum([self.playerPointMap[player] for player in selectedTeam])
             pointsCriteria = (totalPoints <= self.maxPoints)
-            
             uniqueTeamCriteria = (np.sum([set(team)==set(selectedTeam) for team in selectedTeams]) == 0)
-            
             minPlayersCriteria = (teamDemography[self.team1]>=self.teamParams['min']) and (teamDemography[self.team2]>=self.teamParams['min']) 
-            
             maxPlayersCriteria = (teamDemography[self.team1]<=self.teamParams['max']) and (teamDemography[self.team2]<=self.teamParams['max'])
             
             if captain:
                 [c, vc] = rnd.sample(selectedTeam, 2)   
                 selectedTeam = [player + " (c)" if player==c else player + " (vc)" if player==vc else player for player in selectedTeam]
-                
-            
-            if  pointsCriteria and uniqueTeamCriteria and minPlayersCriteria and maxPlayersCriteria:
-                
-                selectedTeams.append(selectedTeam)
-                
-                
+                            
+            if  pointsCriteria and uniqueTeamCriteria and minPlayersCriteria and maxPlayersCriteria:                
+                selectedTeams.append(selectedTeam)                                
                 selected = selected + 1
                 
                 print("------------ Team", selected, "------------")
