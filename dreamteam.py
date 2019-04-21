8c2fdd9ddf4405279486636096930be3914b5283
Manish Chalana <chalana.manish@gmail.com>
2019-04-21 14:55:27 +0530

    Player points based softmax probabilities for player selection, added the similarity index logic to select dissimilar teams, clean output presentation of selected teams, code cleanup

--- dreamteam.py
+++ dreamteam.py
@@ -7,8 +7,9 @@ Created on Sat Apr 20 02:58:47 2019
 """
 
 import numpy as np
-import random as rnd
 import collections
+import pandas as pd
+
 
 
 class Team:
@@ -21,62 +22,180 @@ class Team:
         
         
 class Dream11(Team):
+    
     def __init__(self, players, teams):
         super().__init__(players, teams)
         self.teamParams = {'min':4, 'max':7}
         self.maxPoints = 100
-        self.combinations = [[1,3,2,5], [1,3,3,4], [1,3,4,3], [1,4,1,5], [1,4,2,4], [1,4,3,3], [1,5,1,4], [1,5,2,3]]
+        self.combinations = [[1,3,2,5], [1,3,3,4], [1,4,1,5], [1,4,2,4], [1,4,3,3], [1,5,1,4], [1,5,2,3]]
+#        self.combinationsProb = [0.14]*6 + [0.16] 
+#        self.combinationsProb = [0.1,0.2,0.05,0.2,0.3,0.05,0.1]
+        self.playerNames = [p['name'] for p in players]
         self.playerPointMap = {p['name']:p['points'] for p in self.players}
         self.playerTeamMap = {p['name']:p['team'] for p in self.players}
         self.playerTypeMap = {p['name']:p['type'] for p in self.players}
         self.playerTypeCount = collections.Counter(self.playerTypeMap.values())
                 
     @staticmethod
-    def sample(players, req):        
+    def getSoftmaxProbabilities(points, toneDownMultiplier=1):
+        if len(points) == 0:
+            return []
+        points = [toneDownMultiplier*point for point in points]
+        denom = np.sum([np.exp(point) for point in points])
+        softMax = [np.exp(point)/denom for point in points]
+        return softMax
+    
+    
+    def sample(self, players, req): 
         numStars = np.sum([p['star'] for p in players])
+        
         availableStars = [p['name'] for p in players if p['star']==1]
+        availableStarsPoints = [p['points'] for p in players if p['star']==1]
+        availableStarsSoftMax = Dream11.getSoftmaxProbabilities(availableStarsPoints)
+        
         availableNonStars = [p['name'] for p in players if not(p['star']==1)]
+        availableNonStarsPoints = [p['points'] for p in players if not(p['star']==1)]
+        availableNonStarsSoftMax = Dream11.getSoftmaxProbabilities(availableNonStarsPoints)
     
         if numStars >= req:
-            return ([] if len(availableStars)==0 else rnd.sample(availableStars, req))
+            return ([] if len(availableStars)==0 else list(np.random.choice(availableStars, size = req, p=availableStarsSoftMax, replace=False)))
         else:
-            return availableStars + rnd.sample(availableNonStars, req - numStars)        
+            return availableStars + list(np.random.choice(availableNonStars, size = req - numStars, p=availableNonStarsSoftMax, replace=False))      
+    
+    def calculateTeamVector(self, team):
+        teamVector = [player['points'] if (player['name'] in team) else 0 for player in self.players]
+        return teamVector
+
+    def checkUniqueTeamCriteria(self, sampledTeam, selectedTeams):
+        return (np.sum([set(team)==set(sampledTeam) for team in selectedTeams]) == 0)
+    
+    def checkMinPlayersTeamCriteria(self, teamDemography):
+        return (teamDemography[self.team1]>=self.teamParams['min']) and (teamDemography[self.team2]>=self.teamParams['min']) 
+    
+    def checkMaxPlayersTeamCriteria(self, teamDemography):
+        return (teamDemography[self.team1]<=self.teamParams['max']) and (teamDemography[self.team2]<=self.teamParams['max'])
+    
+    def checkMaxTeamPointsCriteria(self, sampledTeam):
+        totalPoints = np.sum([self.playerPointMap[player] for player in sampledTeam])
+        return (totalPoints <= self.maxPoints)
+        
+    def checkSimilarityIndexCriteria(self, sampledTeam, selectedTeams, selected, thresh=24):
+        
+        similarityIndexCriteria = False
+        similarityIndices = []
+        
+        if selected>0:
+            similarityIndices = [np.sqrt(np.sum(np.square(np.array(self.calculateTeamVector(sampledTeam)) - np.array(self.calculateTeamVector(team))))) for team in selectedTeams]
+            if np.min(similarityIndices) > thresh:
+                similarityIndexCriteria = True
+        else:
+            similarityIndexCriteria = True
+        return similarityIndices, similarityIndexCriteria
+    
+    def chooseCaptain(self, sampledTeam, captain):
+        [c, vc] = ['', '']
+        if captain:
+            sampledTeamSoftmax = [self.playerPointMap[name] for name in sampledTeam]
+            [c, vc] = np.random.choice(sampledTeam, size=2, replace=False, p=Dream11.getSoftmaxProbabilities(sampledTeamSoftmax, toneDownMultiplier=0.75))   
+            
+        return [c, vc]
+
+    def updateSelectedTeams(self, sampledTeam, selectedTeams, selected, captain):
+        #Finding the numbers of wks, bats, ar and bowls in the sampled team
+        teamDemography = collections.Counter([self.playerTeamMap[player] for player in sampledTeam])
+        
+        # Finding the total number of points spent in selecting the sampled team and ensuring that it is less than maximum available points
+        pointsCriteria = self.checkMaxTeamPointsCriteria(sampledTeam)
+        
+        # Ensuring that sampled team is not same as one of the previously selected teams
+        uniqueTeamCriteria = self.checkUniqueTeamCriteria(sampledTeam, selectedTeams)
+        
+        # Making sure that minimum required players are selected in sampled team from each of the two parent teams
+        minPlayersCriteria = self.checkMinPlayersTeamCriteria(teamDemography)
+        
+        # Making sure that maximum required players are selected in sampled team from each of the two parent teams
+        maxPlayersCriteria = self.checkMaxPlayersTeamCriteria(teamDemography)
+        
+        # Calculating the similarity index of the sampled team with the already selected teams to ensure dissimilarity
+        similarityIndices, similarityIndexCriteria = self.checkSimilarityIndexCriteria(sampledTeam, selectedTeams, selected)
+        
+        # Choosing a captain and vice captain
+        [c, vc] = self.chooseCaptain(sampledTeam, captain)
+        
+        if  pointsCriteria and uniqueTeamCriteria and minPlayersCriteria and maxPlayersCriteria and similarityIndexCriteria:                
+            
+            # When all criterias are fulfilled by the team, append the sampled team to list of already selected teams
+            selectedTeams.append(sampledTeam)                                
+            selected = selected + 1
+            
+            self.printTeamDetails(sampledTeam, selected, c, vc, similarityIndices)
+            
+            # Finding the attributes of the selected team
+            
+        return selectedTeams, selected
+    
+    
+    def printTeamDetails(self, sampledTeam, index, c, vc, similarityIndices):
+        
+        selectedPlayersNames = [name.upper() for name in sampledTeam]
+        selectedPlayersPoints = [[p for p in self.players if p['name']==name][0]['points'] for name in sampledTeam]
+        selectedPlayersTeams = [[p for p in self.players if p['name']==name][0]['team'].upper() for name in sampledTeam]
+        selectedPlayersTypes = [[p for p in self.players if p['name']==name][0]['type'] for name in sampledTeam]
+        selectedPlayersCVC = ['c' if name==c else 'vc' if name==vc else ' ' for name in sampledTeam ]
+        selectedPlayersTypesCounter = collections.Counter(selectedPlayersTypes)
+        selectedPlayersTeamsCounter = collections.Counter(selectedPlayersTeams)
+            
+        # Defining a dataframe containing the details of sampled team
+        teamDf = pd.DataFrame({'Player': selectedPlayersNames, 'C/VC': selectedPlayersCVC, 'Points':selectedPlayersPoints, 'Team':selectedPlayersTeams, 'Type':selectedPlayersTypes}, index=range(1,12))[['Player', 'C/VC', 'Team', 'Type', 'Points']]
+        teamDf = pd.concat([teamDf[teamDf['Type']==playType].sort_values(['Points'], ascending=False) for playType in ['wk', 'bat', 'ar', 'bowl']])
+        teamDf.index = range(1,12)
+            
+        # Printing the team on console
+        print("------------ Team", index, "------------\n")
+        print(teamDf)
+        print("\nTotal points invested:", np.sum(teamDf['Points']))
+        print("Wicket-keepers:", selectedPlayersTypesCounter['wk'], ", Batsman:", selectedPlayersTypesCounter['bat'], ", All-rounders:", selectedPlayersTypesCounter['ar'], " Bowlers:", selectedPlayersTypesCounter['bowl'])
+        print(self.team1.upper(), ":", selectedPlayersTeamsCounter[self.team1.upper()], self.team2.upper(), ":", selectedPlayersTeamsCounter[self.team2.upper()])
+        print("Similarity indices:", similarityIndices)
+        print('\n')
+        
+    
+    
     
     def generateTeams(self, numTeams, captain=False):
         selectedTeams = []
         selected = 0
         
         while selected < numTeams:
+            # Choose a random combination of number of wicket-keepers, batsman, all-rounders and bowlers
             while True:
-                [numWks, numBats, numArs, numBowls] = rnd.sample(self.combinations, 1)[0]
+                [numWks, numBats, numArs, numBowls] = self.combinations[np.random.choice(len(self.combinations), size=1)[0]]
                 if (numWks <= self.playerTypeCount['wk']) and (numBats <=self.playerTypeCount['bat']) and (numArs <=self.playerTypeCount['ar']) and (numBowls<=self.playerTypeCount['bowl']):
                     break
-                    
-            wks = Dream11.sample([p for p in self.players if p['type']=='wk'], numWks)
-            bats = Dream11.sample([p for p in self.players if p['type']=='bat'], numBats)
-            ars = Dream11.sample([p for p in self.players if p['type']=='ar'], numArs)
-            bowls = Dream11.sample([p for p in self.players if p['type']=='bowl'], numBowls)
-            selectedTeam = wks + bats + ars + bowls
             
-            teamDemography = collections.Counter([self.playerTeamMap[player] for player in selectedTeam])
-            totalPoints = np.sum([self.playerPointMap[player] for player in selectedTeam])
-            pointsCriteria = (totalPoints <= self.maxPoints)
-            uniqueTeamCriteria = (np.sum([set(team)==set(selectedTeam) for team in selectedTeams]) == 0)
-            minPlayersCriteria = (teamDemography[self.team1]>=self.teamParams['min']) and (teamDemography[self.team2]>=self.teamParams['min']) 
-            maxPlayersCriteria = (teamDemography[self.team1]<=self.teamParams['max']) and (teamDemography[self.team2]<=self.teamParams['max'])
+            # Choose Keepers
+            wks = self.sample([p for p in self.players if p['type']=='wk'], numWks)
             
-            if captain:
-                [c, vc] = rnd.sample(selectedTeam, 2)   
-                selectedTeam = [player + " (c)" if player==c else player + " (vc)" if player==vc else player for player in selectedTeam]
+            # Choose batsman
+            bats = self.sample([p for p in self.players if p['type']=='bat'], numBats)
+            
+            # Choose all-rounders
+            ars = self.sample([p for p in self.players if p['type']=='ar'], numArs)
+            
+            # Choose bowlers
+            bowls = self.sample([p for p in self.players if p['type']=='bowl'], numBowls)
+            
+            # Chosen team
+            sampledTeam = wks + bats + ars + bowls
+            
+            # Update selected teams list based on whether the sampled team fulfills all eligibility criteria
+            selectedTeams, selected = self.updateSelectedTeams(sampledTeam, selectedTeams, selected, captain)
                             
-            if  pointsCriteria and uniqueTeamCriteria and minPlayersCriteria and maxPlayersCriteria:                
-                selectedTeams.append(selectedTeam)                                
-                selected = selected + 1
+            
                 
-                print("------------ Team", selected, "------------")
-                for i in range(11):
-                    print(str(i+1) +  '.', selectedTeam[i])
                 
-        return selectedTeams
+                
+                
+        
     
     
--- main.py
+++ main.py
@@ -9,32 +9,94 @@ wk = 'wk'
 ar = 'ar'
 
 # ('player name')
-t1 = 'mi'
-t2 = 'rr'
-numOfTeams = 5
+t1 = 'srh'
+t2 = 'kkr'
+numOfTeams = 8
 
-p1 = {'name': 'Q Kock', 'type':wk, 'points': 9, 'team': t1, 'star':1}
-p2 = {'name': 'R Sharma', 'type':bat, 'points': 10, 'team': t1, 'star':1}
-p3 = {'name': 'S Yadav', 'type':bat, 'points': 9, 'team': t1, 'star':0}
-p4 = {'name': 'I Kishan', 'type':bat, 'points': 8.5, 'team': t1, 'star':0}
-p5 = {'name': 'H Pandya', 'type':ar, 'points': 9.5, 'team': t1, 'star':1}
-p6 = {'name': 'K Pandya', 'type':ar, 'points': 8.5, 'team': t1, 'star':0}
-p7 = {'name': 'K Pollard', 'type':bat, 'points': 9, 'team': t1, 'star':1}
-p8 = {'name': 'J Behrendorff', 'type':bowl, 'points': 8.5, 'team': t1, 'star':0}
-p9 = {'name': 'R Chahar', 'type':bowl, 'points': 8, 'team': t1, 'star':0}
-p10 = {'name': 'A Joseph', 'type':bowl, 'points': 8.5, 'team': t1, 'star':0}
-p11 = {'name': 'J Bumrah', 'type':bowl, 'points': 9, 'team': t1, 'star':1}
-p12 = {'name': 'A Rahane', 'type':bat, 'points': 9, 'team': t2, 'star':0}
-p13 = {'name': 'J Butler', 'type':bat, 'points': 10.5, 'team': t2, 'star':1}
-p14 = {'name': 'S Smith', 'type':bat, 'points': 9, 'team': t2, 'star':1}
-p15 = {'name': 'S Samson', 'type':wk, 'points': 9, 'team': t2, 'star':0}
-p16 = {'name': 'R Tripathi', 'type':bat, 'points': 8.5, 'team': t2, 'star':0}
-p17 = {'name': 'L Livingstone', 'type':bat, 'points': 8, 'team': t2, 'star':0}
-p18 = {'name': 'K Gowtham', 'type':ar, 'points': 8.5, 'team': t2, 'star':0}
-p19 = {'name': 'J Archer', 'type':bowl, 'points': 9, 'team': t2, 'star':1}
-p20 = {'name': 'S Gopal', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}
-p21 = {'name': 'J Unadkat', 'type':bowl, 'points': 8, 'team': t2, 'star':0}
-p22 = {'name': 'D Kulkarni', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}
+
+
+p1 = {'name': 'J Bairstow', 'type':wk, 'points':10 , 'team': t1, 'star':1}#
+p2 = {'name': 'D Warner', 'type':bat, 'points': 11, 'team': t1, 'star':1}#
+p3 = {'name': 'Y Pathan', 'type':bat, 'points': 8, 'team': t1, 'star':0}#
+p4 = {'name': 'M Pandey', 'type':bat, 'points': 8.5, 'team': t1, 'star':0}#
+p5 = {'name': 'V Shankar', 'type':ar, 'points': 8.5, 'team': t1, 'star':0}#
+p6 = {'name': 'S Al Hasan', 'type':ar, 'points': 8.5, 'team': t1, 'star':0}#
+p7 = {'name': 'D Hooda', 'type':bat, 'points': 8, 'team': t1, 'star':0}#
+p8 = {'name': 'Rashid Khan', 'type':bowl, 'points': 9, 'team': t1, 'star':0}#
+p9 = {'name': 'Bhuvi', 'type':bowl, 'points': 8.5, 'team': t1, 'star':0}#
+p10 = {'name': 'S Sharma', 'type':bowl, 'points': 8.5, 'team': t1, 'star':0}#
+p11 = {'name': 'S Kaul', 'type':bowl, 'points': 8.5, 'team': t1, 'star':0}#
+
+
+p12 = {'name': 'C lynn', 'type':bat, 'points': 9.5, 'team': t2, 'star':0}#
+p13 = {'name': 'N Rana', 'type':bat, 'points': 9, 'team': t2, 'star':0}#
+p14 = {'name': 'R Uthappa', 'type':bat, 'points': 9, 'team': t2, 'star':0}#
+p15 = {'name': 'D Karthik', 'type':wk, 'points': 9, 'team': t2, 'star':0}#
+p16 = {'name': 'S Gill', 'type':bat, 'points': 8, 'team': t2, 'star':0}#
+p17 = {'name': 'S Narine', 'type':ar, 'points': 9, 'team': t2, 'star':0}#
+p18 = {'name': 'A Russell', 'type':ar, 'points': 10.5, 'team': t2, 'star':1}#
+p19 = {'name': 'P Chawla', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}#
+p20 = {'name': 'K Yadav', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}#
+p21 = {'name': 'L Ferguson', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}#
+p22 = {'name': 'P Krishna', 'type':bowl, 'points': 8, 'team': t2, 'star':0}#    
+
+
+
+
+
+#p1 = {'name': 'Q Kock', 'type':wk, 'points': 9, 'team': t1, 'star':0}
+#p2 = {'name': 'R Sharma', 'type':bat, 'points': 10, 'team': t1, 'star':1}
+#p3 = {'name': 'S Yadav', 'type':bat, 'points': 9, 'team': t1, 'star':0}
+#p4 = {'name': 'I Kishan', 'type':bat, 'points': 8.5, 'team': t1, 'star':0}
+#p5 = {'name': 'H Pandya', 'type':ar, 'points': 9.5, 'team': t1, 'star':1}
+#p6 = {'name': 'K Pandya', 'type':ar, 'points': 8.5, 'team': t1, 'star':0}
+#p7 = {'name': 'K Pollard', 'type':bat, 'points': 9, 'team': t1, 'star':0}
+#p8 = {'name': 'J Behrendorff', 'type':bowl, 'points': 8.5, 'team': t1, 'star':0}
+#p9 = {'name': 'R Chahar', 'type':bowl, 'points': 8, 'team': t1, 'star':1}
+#p10 = {'name': 'A Joseph', 'type':bowl, 'points': 8.5, 'team': t1, 'star':0}
+#p11 = {'name': 'J Bumrah', 'type':bowl, 'points': 9, 'team': t1, 'star':1}
+#
+#
+#p12 = {'name': 'A Rahane', 'type':bat, 'points': 9, 'team': t2, 'star':0}
+#p13 = {'name': 'J Butler', 'type':bat, 'points': 10.5, 'team': t2, 'star':0}
+#p14 = {'name': 'S Smith', 'type':bat, 'points': 9, 'team': t2, 'star':1}
+#p15 = {'name': 'S Samson', 'type':wk, 'points': 9, 'team': t2, 'star':0}
+#p16 = {'name': 'R Tripathi', 'type':bat, 'points': 8.5, 'team': t2, 'star':0}
+#p17 = {'name': 'L Livingstone', 'type':bat, 'points': 8, 'team': t2, 'star':0}
+#p18 = {'name': 'K Gowtham', 'type':ar, 'points': 8.5, 'team': t2, 'star':0}
+#p19 = {'name': 'J Archer', 'type':bowl, 'points': 9, 'team': t2, 'star':0}
+#p20 = {'name': 'S Gopal', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}
+#p21 = {'name': 'J Unadkat', 'type':bowl, 'points': 8, 'team': t2, 'star':0}
+#p22 = {'name': 'D Kulkarni', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}
+
+
+
+
+
+#p1 = {'name': 'KL Rahul',     'type':wk,     'points': 10.5,     'team': t1, 'star':0}
+#p2 = {'name': 'C Gayle',      'type':bat,    'points': 10.5,     'team': t1, 'star':0}
+#p3 = {'name': 'M Agarwal',    'type':bat,    'points': 9,        'team': t1, 'star':0}
+#p4 = {'name': 'D Miller',     'type':bat,    'points': 9,        'team': t1, 'star':0}
+#p5 = {'name': 'R Ashwin',     'type':ar,     'points': 9,        'team': t1, 'star':0}
+#p6 = {'name': 'S Curran',     'type':ar,     'points': 8.5,      'team': t1, 'star':0}
+#p7 = {'name': 'M Singh',      'type':bat,    'points': 8,        'team': t1, 'star':0}
+#p8 = {'name': 'M Shami',      'type':bowl,   'points': 8.5,      'team': t1, 'star':0}
+#p9 = {'name': 'H Viljoen',    'type':bowl,   'points': 8.5,      'team': t1, 'star':0}
+#p10 = {'name': 'H Brar',      'type':ar,     'points': 7.5,      'team': t1, 'star':0}
+#p11 = {'name': 'M Ashwin',    'type':bowl,   'points': 8,        'team': t1, 'star':0}
+#
+#
+#p12 = {'name': 'R Pant',      'type':wk,     'points': 9.5,      'team': t2, 'star':0}
+#p13 = {'name': 'S Rutherford','type':bat,    'points': 8,        'team': t2, 'star':0}
+#p14 = {'name': 'S Dhawan',    'type':bat,    'points': 9.5,      'team': t2, 'star':0}
+#p15 = {'name': 'S Iyer',      'type':bat,    'points': 9,        'team': t2, 'star':0}
+#p16 = {'name': 'P Shaw',      'type':bat,    'points': 8.5,      'team': t2, 'star':0}
+#p17 = {'name': 'C Ingram',    'type':bat,    'points': 8.5,      'team': t2, 'star':0}
+#p18 = {'name': 'A Patel',     'type':ar,     'points': 8.5,      'team': t2, 'star':0}
+#p19 = {'name': 'K Rabada',    'type':bowl,   'points': 9.5,      'team': t2, 'star':0}
+#p20 = {'name': 'A Mishra',    'type':bowl,   'points': 8.5,      'team': t2, 'star':0}
+#p21 = {'name': 'I Sharma',    'type':bowl,   'points': 8.5,      'team': t2, 'star':0}
+#p22 = {'name': 'S Lamichane', 'type':bowl,   'points': 8.5,      'team': t2, 'star':0}
 
 
 players = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11,
@@ -45,8 +107,7 @@ from dreamteam import Dream11
 
 
 if __name__ == '__main__':        
-    myTeam = Dream11(players, teams)  
-    teams = myTeam.generateTeams(numOfTeams, captain=True) 
+    Dream11(players, teams).generateTeams(numOfTeams, captain=True) 
 
     
 
