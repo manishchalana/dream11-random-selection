370d377ef5ebb89113f8b1188a69432b0854095c
Manish Chalana <chalana.manish@gmail.com>
2019-04-21 15:01:15 +0530

    Cleaned up the file main.py, No code change

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ bowl = 'bowl'
 wk = 'wk'
 ar = 'ar'
 
-# ('player name')
+
 t1 = 'srh'
 t2 = 'kkr'
 numOfTeams = 8
 
-
+# ('player name', 'player role', 'player points in dream11 app', 'player team', 'star player i.e. identifier to denote whether must be included in the team or not (if star =1, player will be included in the team'))
 
 p1 = {'name': 'J Bairstow', 'type':wk, 'points':10 , 'team': t1, 'star':1}#
 p2 = {'name': 'D Warner', 'type':bat, 'points': 11, 'team': t1, 'star':1}#
@@ -39,65 +39,6 @@ p19 = {'name': 'P Chawla', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}#
 p20 = {'name': 'K Yadav', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}#
 p21 = {'name': 'L Ferguson', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}#
 p22 = {'name': 'P Krishna', 'type':bowl, 'points': 8, 'team': t2, 'star':0}#    
-
-
-
-
-
-#p1 = {'name': 'Q Kock', 'type':wk, 'points': 9, 'team': t1, 'star':0}
-#p2 = {'name': 'R Sharma', 'type':bat, 'points': 10, 'team': t1, 'star':1}
-#p3 = {'name': 'S Yadav', 'type':bat, 'points': 9, 'team': t1, 'star':0}
-#p4 = {'name': 'I Kishan', 'type':bat, 'points': 8.5, 'team': t1, 'star':0}
-#p5 = {'name': 'H Pandya', 'type':ar, 'points': 9.5, 'team': t1, 'star':1}
-#p6 = {'name': 'K Pandya', 'type':ar, 'points': 8.5, 'team': t1, 'star':0}
-#p7 = {'name': 'K Pollard', 'type':bat, 'points': 9, 'team': t1, 'star':0}
-#p8 = {'name': 'J Behrendorff', 'type':bowl, 'points': 8.5, 'team': t1, 'star':0}
-#p9 = {'name': 'R Chahar', 'type':bowl, 'points': 8, 'team': t1, 'star':1}
-#p10 = {'name': 'A Joseph', 'type':bowl, 'points': 8.5, 'team': t1, 'star':0}
-#p11 = {'name': 'J Bumrah', 'type':bowl, 'points': 9, 'team': t1, 'star':1}
-#
-#
-#p12 = {'name': 'A Rahane', 'type':bat, 'points': 9, 'team': t2, 'star':0}
-#p13 = {'name': 'J Butler', 'type':bat, 'points': 10.5, 'team': t2, 'star':0}
-#p14 = {'name': 'S Smith', 'type':bat, 'points': 9, 'team': t2, 'star':1}
-#p15 = {'name': 'S Samson', 'type':wk, 'points': 9, 'team': t2, 'star':0}
-#p16 = {'name': 'R Tripathi', 'type':bat, 'points': 8.5, 'team': t2, 'star':0}
-#p17 = {'name': 'L Livingstone', 'type':bat, 'points': 8, 'team': t2, 'star':0}
-#p18 = {'name': 'K Gowtham', 'type':ar, 'points': 8.5, 'team': t2, 'star':0}
-#p19 = {'name': 'J Archer', 'type':bowl, 'points': 9, 'team': t2, 'star':0}
-#p20 = {'name': 'S Gopal', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}
-#p21 = {'name': 'J Unadkat', 'type':bowl, 'points': 8, 'team': t2, 'star':0}
-#p22 = {'name': 'D Kulkarni', 'type':bowl, 'points': 8.5, 'team': t2, 'star':0}
-
-
-
-
-
-#p1 = {'name': 'KL Rahul',     'type':wk,     'points': 10.5,     'team': t1, 'star':0}
-#p2 = {'name': 'C Gayle',      'type':bat,    'points': 10.5,     'team': t1, 'star':0}
-#p3 = {'name': 'M Agarwal',    'type':bat,    'points': 9,        'team': t1, 'star':0}
-#p4 = {'name': 'D Miller',     'type':bat,    'points': 9,        'team': t1, 'star':0}
-#p5 = {'name': 'R Ashwin',     'type':ar,     'points': 9,        'team': t1, 'star':0}
-#p6 = {'name': 'S Curran',     'type':ar,     'points': 8.5,      'team': t1, 'star':0}
-#p7 = {'name': 'M Singh',      'type':bat,    'points': 8,        'team': t1, 'star':0}
-#p8 = {'name': 'M Shami',      'type':bowl,   'points': 8.5,      'team': t1, 'star':0}
-#p9 = {'name': 'H Viljoen',    'type':bowl,   'points': 8.5,      'team': t1, 'star':0}
-#p10 = {'name': 'H Brar',      'type':ar,     'points': 7.5,      'team': t1, 'star':0}
-#p11 = {'name': 'M Ashwin',    'type':bowl,   'points': 8,        'team': t1, 'star':0}
-#
-#
-#p12 = {'name': 'R Pant',      'type':wk,     'points': 9.5,      'team': t2, 'star':0}
-#p13 = {'name': 'S Rutherford','type':bat,    'points': 8,        'team': t2, 'star':0}
-#p14 = {'name': 'S Dhawan',    'type':bat,    'points': 9.5,      'team': t2, 'star':0}
-#p15 = {'name': 'S Iyer',      'type':bat,    'points': 9,        'team': t2, 'star':0}
-#p16 = {'name': 'P Shaw',      'type':bat,    'points': 8.5,      'team': t2, 'star':0}
-#p17 = {'name': 'C Ingram',    'type':bat,    'points': 8.5,      'team': t2, 'star':0}
-#p18 = {'name': 'A Patel',     'type':ar,     'points': 8.5,      'team': t2, 'star':0}
-#p19 = {'name': 'K Rabada',    'type':bowl,   'points': 9.5,      'team': t2, 'star':0}
-#p20 = {'name': 'A Mishra',    'type':bowl,   'points': 8.5,      'team': t2, 'star':0}
-#p21 = {'name': 'I Sharma',    'type':bowl,   'points': 8.5,      'team': t2, 'star':0}
-#p22 = {'name': 'S Lamichane', 'type':bowl,   'points': 8.5,      'team': t2, 'star':0}
-
 
 players = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11,
            p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22]
